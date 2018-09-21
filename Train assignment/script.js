a59bd3edc0d7683c29124da650d704e1a51604d8
iankimble <ian.kimble571@gmail.com>
2018-09-21 00:43:33 -0400

    homework_assignment

---
+++ Train assignment/script.js
@@ -0,0 +1,18 @@
+var appInfo = {};
+(function(){
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAKXerZg_zc6B8XltNl0idimuQ_cqDIMWY",
+    authDomain: "ian-kimble-train-project.firebaseapp.com",
+    databaseURL: "https://ian-kimble-train-project.firebaseio.com",
+    projectId: "ian-kimble-train-project",
+    storageBucket: "ian-kimble-train-project.appspot.com",
+    messagingSenderId: "798524304933"
+  };
+  firebase.initializeApp(config);
+
+  appInfo= firebase;
+})()
+
+console.log(link);
